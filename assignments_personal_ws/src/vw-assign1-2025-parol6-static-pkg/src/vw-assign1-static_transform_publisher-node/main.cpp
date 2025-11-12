31bfb0a17667e2f8d3047172be1090ec9e8d9be2
SirDeP <biogene.53418@gmail.com>
2025-11-12 09:42:05 +0100

    feat(assign2): Remove assign1 and create assign2

--- assignments_personal_ws/src/vw-assign1-2025-parol6-static-pkg/src/vw-assign1-static_transform_publisher-node/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "nodes/static_transform_publisher.hpp"
-
-int main(int argc, char** argv) {
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<StaticTfNode>());
-  rclcpp::shutdown();
-  return 0;
-}
